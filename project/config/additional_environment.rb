aad370acb26d5edede3c17bd60b122da7d943f43
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2016-11-14 18:16:31 +0200

    Progress on new docker compose setup

--- project/config/additional_environment.rb
+++ project/config/additional_environment.rb
@@ -1,8 +1,11 @@
 if Rails.env.development?
   config.logger = Logger.new(STDOUT)
-  logger_level = ENV.fetch('LOG_LEVEL', 'DEBUG').upcase
+  logger_level  = ENV.fetch('LOG_LEVEL', 'DEBUG').upcase
+
   config.logger.level = Logger.const_get(logger_level)
+
   config.active_record.schema_format = :ruby
+
   config.web_console.whitelisted_ips = ['172.16.0.0/12', '192.168.0.0/16', '10.0.2.2']
 end
 
